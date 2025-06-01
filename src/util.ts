ee379e0d5f3aa22450ee11ba0c4dc4f708edb37d
Gnlow <gnlow@pm.me>
2025-06-02 00:27:08 +0900

    mat

--- src/util.ts
+++ src/util.ts
@@ -32,3 +32,55 @@ export const ns2n =
 export const arr =
 (length: number) =>
     Array.from({ length }, (_, i) => i)
+
+export class Mat {
+    dimension
+    data
+    constructor(dimension: number[], data: number[]) {
+        this.dimension = dimension
+        this.data = data
+    }
+
+    ns2n(ns: number[]) { return ns2n(this.dimension)(ns) }
+    n2ns(n: number) { return n2ns(this.dimension)(n) }
+
+    get(is: number[]) {
+        return this.data[this.ns2n(is)]
+    }
+    set(is: number[], value: number) {
+        this.data[this.ns2n(is)] = value
+    }
+    map(f: (value: number, is: number[]) => number) {
+        return new Mat(
+            this.dimension,
+            this.data.map((v, i) => f(v, this.n2ns(i)))
+        )
+    }
+    forEach(f: (value: number, is: number[]) => number) {
+        this.map(f)
+    }
+    reduce(
+        f: (prev: number, value: number, is: number[]) => number,
+    ): number
+    reduce<T>(
+        f: (prev: T, value: number, is: number[]) => T,
+        init: T,
+    ): T
+    reduce<T>(
+        f: (prev: T, value: number, is: number[]) => T,
+        init?: T,
+    ): T | number {
+        if (init) {
+            return this.data.reduce(
+                (prev, value, i) =>
+                    f(prev, value, this.n2ns(i)),
+                init as T,
+            )
+        } else {
+            return this.data.reduce(
+                (prev, value, i) =>
+                    (f as unknown as (prev: number, value: number, is: number[]) => number)(prev, value, this.n2ns(i)),
+            )
+        }
+    }
+}
